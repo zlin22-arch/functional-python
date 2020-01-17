5868a2339315fa7e404077e7050f8bc8dbb85047
Mike Izbicki <mike@izbicki.me>
2020-01-17 15:13:22 -0800

    upload assignment material

---
+++ Fraction.py
@@ -0,0 +1,42 @@
+'''
+This code comes from https://runestone.academy/runestone/books/published/pythonds/Introduction/ObjectOrientedProgramminginPythonDefiningClasses.html#a-fraction-class
+'''
+
+def gcd(m,n):
+    while m%n != 0:
+        oldm = m
+        oldn = n
+
+        m = oldn
+        n = oldm%oldn
+    return n
+
+class Fraction:
+    def __init__(self,top,bottom):
+        self.num = top
+        self.den = bottom
+
+    def __str__(self):
+        return str(self.num)+"/"+str(self.den)
+
+    def show(self):
+        print(self.num,"/",self.den)
+
+    def __add__(self,otherfraction):
+        newnum = self.num*otherfraction.den + \
+                     self.den*otherfraction.num
+        newden = self.den * otherfraction.den
+        common = gcd(newnum,newden)
+        return Fraction(newnum//common,newden//common)
+
+    def __eq__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+
+        return firstnum == secondnum
+
+    def __mul__(self, other):
+        num = self.num * other.num
+        den = self.den * other.den
+        common = gcd(num,den)
+        return Fraction(num//common,den//common)
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# cmc-csci046-hw1
+# CMC-CSCI046 Homework 1
+
+**Learning Objectives:**
+
+1. Understand the open source tool chain
+1. Be able to implement Python classes
+
+## Procedure
+
+1. Fork this repository
+1. Implement the `__mul__`, `__div__`, and `__sub__` functions within the `Fraction` class 
+1. Run the command `python3 -m pytest` to ensure that all test cases pass
+1. Enable Travis for this repository
+    1. You can follow the tutorial [here](https://docs.travis-ci.com/user/tutorial/)
+1. Push your project onto github
+
+## Grading
+
+
