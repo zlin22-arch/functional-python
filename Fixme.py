cf8492f28df481a28b39ffc2ce4929cce61aec61
Mike Izbicki <mike@izbicki.me>
2021-01-28 12:20:17 -0800

    create assignment

--- Fixme.py
+++ Fixme.py
@@ -1,44 +1,125 @@
 #!/usr/bin/python3
 
-def factorial(n):
+def evens(n):
     '''
-    Returns the product of all numbers from 1 to n.
+    Returns a list of even numbers from 0 to n inclusive.
 
-    >>> factorial(1)
-    1
-    >>> factorial(2)
-    2
-    >>> factorial(3)
-    6
-    >>> factorial(4)
-    24
-    >>> factorial(40)
-    815915283247897734345611269596115894272000000000
-    >>> factorial(400)
-    64034522846623895262347970319503005850702583026002959458684445942802397169186831436278478647463264676294350575035856810848298162883517435228961988646802997937341654150838162426461942352307046244325015114448670890662773914918117331955996440709549671345290477020322434911210797593280795101545372667251627877890009349763765710326350331533965349868386831339352024373788157786791506311858702618270169819740062983025308591298346162272304558339520759611505302236086810433297255194852674432232438669948422404232599805551610635942376961399231917134063858996537970147827206606320217379472010321356624613809077942304597360699567595836096158715129913822286578579549361617654480453222007825818400848436415591229454275384803558374518022675900061399560145595206127211192918105032491008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+    NOTE:
+    Your function should be only a single line long.
+    You may not use any loops when implementing this function;
+    you must use either the map and filter functions or list comprehensions.
+
+    >>> evens(10)
+    [0, 2, 4, 6, 8, 10]
+    >>> evens(11)
+    [0, 2, 4, 6, 8, 10]
+    >>> evens(0)
+    [0]
+    >>> evens(1)
+    [0]
+    >>> evens(-1)
+    []
     '''
-    result=1
-    for i in range(2,n+1):
-        result*=i
-    return result
 
-def triangular(n):
+
+def threes(n):
     '''
-    Returns the nth triangular number.
+    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
 
-    The nth triangular number is the sum of all numbers from 1 to n.
-    It is like the factorial, but uses addition instead of multiplication.
+    NOTE:
+    Your function should be only a single line long.
+    You may not use any loops when implementing this function;
+    you must use either the map and filter functions or list comprehensions.
 
-    >>> triangular(1)
-    1
-    >>> triangular(2)
-    3
-    >>> triangular(3)
-    6
-    >>> triangular(4)
-    10
-    >>> triangular(40)
-    820
-    >>> triangular(400)
-    80200
+    >>> threes([2])
+    []
+    >>> threes([3])
+    [3]
+    >>> threes([10])
+    [3]
+    >>> threes([20])
+    [3, 13]
+    >>> threes([50])
+    [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
+    '''
+
+
+def small_words(text):
+    '''
+    Returns a list of all words in the input text that are less than 4 characters long.
+
+    NOTE:
+    Your function should be only a single line long.
+    You may not use any loops when implementing this function;
+    you must use either the map and filter functions or list comprehensions.
+
+    >>> small_words('this is a simple test case')
+    ['this', 'is', 'a', 'test', 'case']
+    >>> small_words('really enormous words')
+    []
+    >>> small_words('')
+    []
+    >>> small_words('a big word is bad')
+    ['a', 'big', 'word', 'is', 'bad']
+    '''
+
+
+def squares(n):
+    '''
+    Returns a list of all square number between 1 and n inclusive.
+    Recall that the nth square number is defined to be n*n.
+
+    NOTE:
+    Your function should be only a single line long.
+    You may not use any loops when implementing this function;
+    you must use either the map and filter functions or list comprehensions.
+
+    >>> squares(1)
+    [1]
+    >>> squares(2)
+    [1, 4]
+    >>> squares(3)
+    [1, 4, 9]
+    >>> squares(10)
+    [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+    '''
+
+
+def lengths(strings):
+    '''
+    Given a list of strings, returns a list of the lengths of the corresponding strings.
+
+    NOTE:
+    Your function should be only a single line long.
+    You may not use any loops when implementing this function;
+    you must use either the map and filter functions or list comprehensions.
+
+    >>> lengths([])
+    []
+    >>> lengths(['test'])
+    [4]
+    >>> lengths(['this','is','a','test'])
+    [4,2,1,4]
+    '''
+
+
+def capital_lengths(strings):
+    '''
+    Given a list of strings, returns a list of the lengths of the corresponding strings that start with capital letters.
+
+    NOTE:
+    Your function should be only a single line long.
+    You may not use any loops when implementing this function;
+    you must use either the map and filter functions or list comprehensions.
+
+    >>> lengths([])
+    []
+    >>> lengths(['test'])
+    []
+    >>> lengths(['this','is','a','test'])
+    []
+    >>> lengths(['This','Is','a','Test'])
+    [4, 2, 4]
+    >>> lengths(['tHIS','Is','a','Test'])
+    [2, 4]
     '''
